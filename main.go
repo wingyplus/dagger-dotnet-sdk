5174b3da2b3ddacb83090eac2c47ce52d756ba31
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-06-05 00:32:18 +0700

    feat: module support
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 // The DotNet SDK and runtime.
 package main
 
+import (
+	"context"
+	_ "embed"
+)
+
+//go:embed introspection.graphql
+var introspectionGraphql string
+
+const introspectionJsonPath = "/introspection.json"
+
+func New(
+	// +optional
+	sdkSourceDir *Directory,
+) *DotnetSdk {
+	if sdkSourceDir == nil {
+		sdkSourceDir = dag.CurrentModule().Source().Directory("sdk")
+	}
+	return &DotnetSdk{
+		SDKSourceDir:  sdkSourceDir,
+		RequiredPaths: []string{},
+		Container:     dag.Container(),
+	}
+}
+
 type DotnetSdk struct {
+	SDKSourceDir  *Directory
+	RequiredPaths []string
+	Container     *Container
+}
+
+func (m *DotnetSdk) ModuleRuntime(
+	ctx context.Context,
+	modSource *ModuleSource,
+	introspectionJson string,
+) (*Container, error) {
+	return nil, nil
+}
+
+func (m *DotnetSdk) Codegen(
+	ctx context.Context,
+	modSource *ModuleSource,
+	introspectionJson string,
+) (*GeneratedCode, error) {
+	return nil, nil
 }
