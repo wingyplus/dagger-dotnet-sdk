2cdadb82be64df464c0e58a0bf42ff93ce8bfb58
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-06-05 22:15:20 +0700

    feat: add fsharp as an example
    This example demonstrate how to integrate our SDK into F#.
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

---
+++ fsharp-sample/FSharpExample/Program.fs
@@ -0,0 +1,15 @@
+﻿open Dagger.SDK
+
+[<EntryPoint>]
+let main args =
+    async {
+        let dag = Dagger.Connect()
+
+        let! output =
+            dag.Container().From("alpine").WithExec([| "echo"; "hello" |]).Stdout()
+            |> Async.AwaitTask
+
+        printfn $"{output}"
+    }
+    |> Async.RunSynchronously
+    exit 0
