57614df0723db55ec78db63bb1df9b75bd992e99
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-06-30 23:48:33 +0700

    feat: my potato dagger functions back to works
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

---
+++ potato/dagger/Potato/Potato_IEntrypoint.cs
@@ -0,0 +1,13 @@
+﻿using Dagger.SDK;
+using Dagger.SDK.Mod;
+
+namespace Potato;
+
+// TODO: move this to source generator.
+public partial class Potato : IEntrypoint
+{
+    public Module Register(Query dag)
+    {
+        return dag.Module().WithObject(ToObjectTypeDef(dag));
+    }
+}
--- potato/dagger/Potato/Program.cs
+++ potato/dagger/Potato/Program.cs
@@ -1,11 +1,36 @@
 ﻿// QUESTION: is it possible to get generate by source generator?
+
+using System.Text.Json;
+using Dagger.SDK;
+using Dagger.SDK.Mod;
+
 public class Program
 {
     public static async Task Main(string[] args)
     {
         var dag = Dagger.SDK.Dagger.Connect();
-        // NOTE: We're in converting from reflection to source generator state. The module is not working
-        // at the moment.
-        // await Dagger.SDK.Mod.Entrypoint.Invoke<Potato.Potato>(dag);
+        await Invoke<Potato.Potato>(dag);
+    }
+
+    private static async Task Invoke<T>(Query dag) where T : class, IDagSetter, IEntrypoint, new()
+    {
+        T root = new();
+        var fnCall = dag.CurrentFunctionCall();
+        var parentName = await fnCall.ParentName();
+        // TODO: Get module name to check root type name match with it.
+
+        var result = parentName switch
+        {
+            // TODO: Dagger.SDK should automatic serialize into id.
+            "" => await root.Register(dag).Id(),
+            _ => throw new Exception($"{parentName} is not supported at the moment.")
+        };
+
+        await fnCall.ReturnValue(IntoJson(result));
+    }
+
+    private static Json IntoJson(object result)
+    {
+        return new Json { Value = JsonSerializer.Serialize(result) };
     }
 }
